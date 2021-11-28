5551861b0dc7b94bc1fc1b6abbd6f6b4c3a78084
=lazofernando <45129156@certus.edu.pe>
2021-11-28 13:09:45 -0500

    desde cero

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 const express = require('express')
-const engine= require('ejs');
 const path = require('path')
 const logger = require('morgan')
-const session = require('express-session');
 
 
 //initilization
 const app = express()
-require('./src/passport/local-auth');
 
 //Routes
 const indexRoutes = require('./src/routes/index');
-const passport = require('passport');
-// const { session } = require('passport');
 // const cashierRouter = require('./models/cashier');
 require('dotenv/config');
 
 //settinggs
 app.set('views', path.join(__dirname, 'src/views'))
-// app.engine('ejs',engine);
 app.set('view engine', 'ejs')
 app.set('port', process.env.PORT || 3001 )
 
@@ -31,13 +25,6 @@ app.set('port', process.env.PORT || 3001 )
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-// app.use(session({
-//     secret : "avengers",
-//     resave : false,
-//     saveUninitialized : false
-// }));
-// app.use(passport.initialize());
-// app.use(passport-session());
 
 
 app.use(express.static(__dirname + '/public'));
