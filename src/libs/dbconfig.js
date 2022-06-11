028a80a8619be5612d6cff1c9fdd641294af0f4f
Jasef21 <jasef.huachambe@gmail.com>
2022-06-11 17:22:22 -0500

    conection BD azure

--- src/libs/dbconfig.js
+++ src/libs/dbconfig.js
@@ -1,13 +1,13 @@
 const config = {
-    user:'sa',
-    password:'123456',
-    //server:'serveravengers.database.windows.net',
-    server:'LAPTOP-3PDIDFO4',
+    user:'adminAvenger',
+    password:'Certus123456',
+    server:'serveravengercertus.database.windows.net',
+    //server:'LAPTOP-3PDIDFO4',
     database: 'DB_INVENTARIO_AVENGERS',
     options:{
         trustedconnection:false,
         enableArithAbort:true,
-        encrypt:false
+        encrypt:true
     }
 }
 
