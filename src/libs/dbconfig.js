7cbcd17e224c5b43264b16cb75a1fc7877b4ad6b
Jasef21 <jasef.huachambe@gmail.com>
2022-06-11 10:50:05 -0500

    conect db local

--- src/libs/dbconfig.js
+++ src/libs/dbconfig.js
@@ -1,12 +1,13 @@
 const config = {
-    user:'avenger',
-    password:'123456xd*',
-    server:'serveravengers.database.windows.net',
+    user:'sa',
+    password:'123456',
+    //server:'serveravengers.database.windows.net',
+    server:'LAPTOP-3PDIDFO4',
     database: 'DB_INVENTARIO_AVENGERS',
     options:{
         trustedconnection:false,
         enableArithAbort:true,
-        encrypt:true
+        encrypt:false
     }
 }
 
